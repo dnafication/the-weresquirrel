74de5bf07f5ff0b7580beb7471647ab0c5b868c4
dnafication <dnafication@users.noreply.github.com>
2019-04-02 16:49:35 +1100

    better looking testkeys :+1:

--- src/pages/testkeys.js
+++ src/pages/testkeys.js
@@ -5,10 +5,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 function TestKeys(props) {
-  const [keyPressed, setKeyPressed] = useState("")
+  const [keyPressed, setKeyPressed] = useState({ key: null })
 
   const handleKeyPress = e => {
-    setKeyPressed(e.key)
+    console.log(e)
+    setKeyPressed(e)
   }
 
   useEffect(() => {
@@ -30,14 +31,39 @@ function TestKeys(props) {
           `eloquentjavascript`,
         ]}
       />
+      <p
+        style={{
+          textAlign: "center",
+        }}
+      >
+        press keys on your keyboard to find out the key code
+      </p>
 
       <div
         id="keyboard"
         style={{
           fontSize: `2em`,
+          textAlign: "center",
+          paddingTop: `25%`,
+          margin: `auto`,
+          height: `10em`,
         }}
       >
-        {keyPressed}
+        {keyPressed.key && (
+          <>
+            {keyPressed.key}
+            <br />
+            <span
+              style={{
+                fontSize: `0.5em`,
+                fontFamily: `monospace`,
+                display: `inline`,
+              }}
+            >
+              code: {keyPressed.keyCode}
+            </span>
+          </>
+        )}
       </div>
     </Layout>
   )
