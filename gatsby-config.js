f17a9da11aa565afd77dd3c33af0efa4835b863c
Dina <dnafication@users.noreply.github.com>
2019-04-04 10:27:17 +1100

    offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,17 +26,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `jacques-the-weresquirrel`,
+        short_name: `the-weresquirrel`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f2cb13`,
+        theme_color: `#f2cb13`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
   ],
 }
